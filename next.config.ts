8e226ed8965e5b9c901bfc23582bcc52fa3b1ecb
Vinicius Périco <105397291+viniciusperico@users.noreply.github.com>
2025-07-01 21:53:08 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: 'export',
   basePath: '/perico',
+  assetPrefix: '/perico',
   typescript: {
     ignoreBuildErrors: true,
   },
